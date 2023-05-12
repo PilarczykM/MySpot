5be41eade2aa65cc6e98e003557b9d5653c048aa
PilarczykM <marcinpilarczyk@me.com>
2023-05-12 12:03:49 +0200

    Appsetings file used to retrive database connection string

--- src/MySpot.Infrastructure/DAL/Extensions.cs
+++ src/MySpot.Infrastructure/DAL/Extensions.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MySpot.Core.Repositories;
 using MySpot.Infrastructure.DAL.Repositories;
@@ -7,10 +8,14 @@ namespace MySpot.Infrastructure.DAL;
 
 internal static class Extensions
 {
-    public static IServiceCollection AddPostgres(this IServiceCollection services)
+    public static IServiceCollection AddPostgres(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
     {
-        const string connectionString =
-            "Host=localhost;Database=my_spot;Username=postgres;Password=";
+        var connectionString = configuration
+            .GetOptions<PostgresOptions>("Postgres")
+            .ConnectionString;
         services
             .AddDbContext<MySpotDbContext>(x => x.UseNpgsql(connectionString))
             .AddScoped<IWeeklyParkingSpotRepository, PostgresWeeklyParkingSpotRepository>();
@@ -20,5 +25,15 @@ internal static class Extensions
         AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
         return services;
+    }
+
+    public static T GetOptions<T>(this IConfiguration configuration, string sectionName)
+        where T : class, new()
+    {
+        var options = new T();
+        var section = configuration.GetSection(sectionName);
+        section.Bind(options);
+
+        return options;
     }
 }
