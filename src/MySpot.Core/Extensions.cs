6e29f2d4de1d807afa1da6b948352295e7a8d462
PilarczykM <marcinpilarczyk@me.com>
2023-05-13 14:01:02 +0200

    Update Extensions.cs
    Register policies

--- src/MySpot.Core/Extensions.cs
+++ src/MySpot.Core/Extensions.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using MySpot.Core.DomainServices;
+using MySpot.Core.Policies;
 
 namespace MySpot.Core
 {
     public static class Extensions
     {
-        public static IServiceCollection AddCore(this IServiceCollection serviceCollection)
+        public static IServiceCollection AddCore(this IServiceCollection services)
         {
-            return serviceCollection;
+            services.AddSingleton<IReservationPolicy, RegularEmployeeReservationPolicy>();
+            services.AddSingleton<IReservationPolicy, ManagerReservationPolicy>();
+            services.AddSingleton<IReservationPolicy, BossReservationPolicy>();
+
+            services.AddSingleton<IParkingReservationService, ParkingReservationService>();
+
+            return services;
         }
     }
 }
