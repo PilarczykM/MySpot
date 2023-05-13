00f1f959161b6ba8264ead55a7dd479c45e02a77
PilarczykM <marcinpilarczyk@me.com>
2023-05-13 14:01:33 +0200

    Reservation service implementation

---
+++ src/MySpot.Core/DomainServices/IParkingReservationService.cs
@@ -0,0 +1,14 @@
+using MySpot.Core.Entities;
+using MySpot.Core.ValueObjects;
+
+namespace MySpot.Core.DomainServices;
+
+public interface IParkingReservationService
+{
+    void ReserveSpotForVehicle(
+        IEnumerable<WeeklyParkingSpot> weeklyParkingSpots,
+        JobTitle jobTitle,
+        WeeklyParkingSpot parkingSpotToReserve,
+        Reservation reservation
+    );
+}
---
+++ src/MySpot.Core/DomainServices/ParkingReservationService.cs
@@ -0,0 +1,45 @@
+using MySpot.Core.Abstractions;
+using MySpot.Core.Entities;
+using MySpot.Core.Exceptions;
+using MySpot.Core.Policies;
+using MySpot.Core.ValueObjects;
+
+namespace MySpot.Core.DomainServices;
+
+internal sealed class ParkingReservationService : IParkingReservationService
+{
+    private readonly IEnumerable<IReservationPolicy> _policies;
+    private readonly IClock _clock;
+
+    public ParkingReservationService(
+        IEnumerable<IReservationPolicy> reservationPolicies,
+        IClock clock
+    )
+    {
+        _policies = reservationPolicies;
+        _clock = clock;
+    }
+
+    public void ReserveSpotForVehicle(
+        IEnumerable<WeeklyParkingSpot> weeklyParkingSpots,
+        JobTitle jobTitle,
+        WeeklyParkingSpot parkingSpotToReserve,
+        Reservation reservation
+    )
+    {
+        var parkingSpotId = parkingSpotToReserve.Id;
+        var policy = _policies.SingleOrDefault(p => p.CanBeApplied(jobTitle));
+
+        if (policy is null)
+        {
+            throw new NoReservationPolicyFoundException(jobTitle);
+        }
+
+        if (!policy.CanReserve(weeklyParkingSpots, reservation.EmployeeName))
+        {
+            throw new CannotReserveParkingSpotException(parkingSpotId);
+        }
+
+        parkingSpotToReserve.AddReservation(reservation, new Date(_clock.Current()));
+    }
+}
