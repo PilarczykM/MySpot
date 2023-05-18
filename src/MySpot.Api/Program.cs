c1ad9c82c96ea733be1277b0cf8648bf4cd1bafb
PilarczykM <marcinpilarczyk@me.com>
2023-05-18 13:44:58 +0200

    Update controllers

--- src/MySpot.Api/Program.cs
+++ src/MySpot.Api/Program.cs
@@ -1,6 +1,9 @@
+using Microsoft.Extensions.Options;
+using MySpot.Api;
 using MySpot.Application;
 using MySpot.Core;
 using MySpot.Infrastructure;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,13 +11,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services
     .AddCore()
     .AddApplication()
-    .AddInfrastructure(builder.Configuration)
-    .AddControllers();
+    .AddInfrastructure(builder.Configuration);
+
+builder.Host.UseSerilog((context, loggerConfiguration) =>
+{
+    loggerConfiguration.WriteTo
+        .Console();
+    // .WriteTo
+    // .File("logs.txt")
+    // .WriteTo
+    // .Seq("http://localhost:5341");
+});
 
 var app = builder.Build();
-
 app.UseInfrastructure();
-
-app.MapControllers();
-
+app.MapGet("api", (IOptions<AppOptions> options) => Results.Ok(options.Value.Name));
+app.UseUsersApi();
 app.Run();
